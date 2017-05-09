18e7e8a1fce9f1f82b30eb5bdb5bb70fcc829a83
Rok Mocnik <rok.mocnik@sinergise.com>
2017-05-09 19:54:56 +0200

    initial result parsing for SICSV

--- ncscoring.py
+++ ncscoring.py
@@ -1,27 +1,35 @@
 # my imports
 import json
 import csv
-import sys
-from pprint import pprint
+from enum import Enum
+from collections import defaultdict
 
-# sys.stdout = codecs.getwriter('utf8')(sys.stdout)
-# print(sys.stdout.encoding)
+class SICsv():
+    CLASSIFIER = 12
+    CLUB = 15
+    NATIONALITY = 16
+    CATEGORY = 18
+    PLACE = 43
+
+class Classifier(Enum):
+    OK = '0'
+    DNS = '1'
+    MP = '3'
 
 # default
 POINTS = [25, 20, 15, 12, 10, 8, 7, 6, 5, 4, 3, 2, 1]
-
+LONG_FILE = "data/long.csv"
 
 # read config
 with open("data/config.json", encoding="utf-8") as config_file:
     CONFIG = json.load(config_file)
 
-# evaluate long
-with open("data/long.csv", encoding="utf-8") as long_file:
-    csvreader = csv.reader(long_file, delimiter=";", quotechar='"')
-    for row in csvreader:
-        print(", " . join(row))
+results = defaultdict(list)
 
-# evaluate relay
-
-# output report
-pprint(CONFIG["long"])
+with open(LONG_FILE, encoding="utf-8") as long_file:
+    for row in csv.reader(long_file, delimiter=";"):
+        if row[SICsv.NATIONALITY] == "SLO" \
+           and row[SICsv.CLASSIFIER] == Classifier.OK.value:
+            key = (row[SICsv.CATEGORY])
+            value = (int(row[SICsv.PLACE]), row[SICsv.CLUB])
+            results[key].append(value)
