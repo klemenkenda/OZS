36a468e3732c2faabe54a92df6da7fafa28d1f9b
Rok Mocnik <rok.mocnik@sinergise.com>
2017-05-10 18:36:38 +0200

    add relay scoring (generalise so it works on both)

--- ncscoring.py
+++ ncscoring.py
@@ -6,7 +6,14 @@ from operator import itemgetter
 from pprint import pprint
 from enum import Enum
 
+DEBUG = False
+
+POINTS = [25, 20, 15, 12, 10, 8, 7, 6, 5, 4, 3, 2, 1]
+LONG_FILE = "data/long.csv"
+RELAY_FILE = "data/relay.csv"
+
 SICSV = {'CLASSIFIER': 12, 'CLUB': 15, 'NATIONALITY': 16, 'CATEGORY': 18, 'PLACE': 43}
+SICSV_RELAY = {'CLASSIFIER': 6, 'CLUB': 9, 'NATIONALITY': 10, 'CATEGORY': 12, 'PLACE': -2}
 
 class Classifier(Enum):
     OK = '0'
@@ -28,31 +35,27 @@ def rank(iterable):
 def get_points(n):
     return POINTS[n] if n < len(POINTS) else POINTS[-1]
 
-DEBUG = False
-# default
-POINTS = [25, 20, 15, 12, 10, 8, 7, 6, 5, 4, 3, 2, 1]
-LONG_FILE = "data/long.csv"
-
 # read config
 with open("data/config.json", encoding="utf-8") as config_file:
     CONFIG = json.load(config_file)
 
-def read_sicsv(file):
+def read_sicsv(file, categories, sicsv):
     results = defaultdict(list)
     with open(file, encoding="utf-8") as long_file:
         for row in csv.reader(long_file, delimiter=";"):
-            if row[SICSV['NATIONALITY']] == "SLO" \
-            and row[SICSV['CLASSIFIER']] == Classifier.OK.value \
-            and row[SICSV['CATEGORY']] in CONFIG['long']:
-                key = (row[SICSV['CATEGORY']])
-                value = (int(row[SICSV['PLACE']]), row[SICSV['CLUB']])
+            if row[sicsv['NATIONALITY']] == "SLO" \
+            and row[sicsv['CLASSIFIER']] == Classifier.OK.value \
+            and row[sicsv['CATEGORY']] in categories:
+                key = (row[sicsv['CATEGORY']])
+                value = (int(row[sicsv['PLACE']]), row[sicsv['CLUB']])
                 results[key].append(value)
+
     if DEBUG:
         print("Results with only national clubs")
         pprint(results)
     return results
 
-def calculate_scores_long(results):
+def calculate_scores(results, runners=2, multiplier=lambda x: 1):
     club_scores = defaultdict(int)
     for category, result in results.items():
         ranking = rank(sorted(result, key=itemgetter(0)))
@@ -60,22 +63,39 @@ def calculate_scores_long(results):
         club_cat_scores = defaultdict(int)
         for points, _, club in ranking:
             counter[club] += 1
-            if counter[club] <= 2:
-                club_cat_scores[club] += points
-                club_scores[club] += points
+            if counter[club] <= runners:
+                club_cat_scores[club] += points * multiplier(category)
+                club_scores[club] += points * multiplier(category)
 
         if DEBUG:
             print(category)
             pprint(ranking)
             pprint(club_cat_scores)
+    if DEBUG:
+        print("Sum scores by club")
+        pprint(club_scores)
     return club_scores
 
 def score_long(long_file):
-    results = read_sicsv(long_file)
-    return calculate_scores_long(results)
+    results = read_sicsv(long_file, CONFIG['long'], SICSV)
+    return calculate_scores(results)
+
+def relay_multiplier(category):
+    return CONFIG['relay']['exceptions'].get(category, CONFIG['relay']['legs'])
+
+def score_relay(relay_file):
+    results = read_sicsv(RELAY_FILE, CONFIG['relay']['categories'], SICSV_RELAY)
+    return calculate_scores(results, 1, relay_multiplier)
+
+def add_scores(score_dict, sum_dict):
+    for k, v in score_dict.items():
+        sum_dict[k] += v
+    return scores
 
 if __name__ == "__main__":
-    scores = score_long(LONG_FILE)
-    
+    scores = defaultdict(int)
+    add_scores(score_long(LONG_FILE), scores)
+    add_scores(score_relay(RELAY_FILE), scores)
+
     for club, score in sorted(scores.items(), key=itemgetter(1), reverse=True):
         print("{0:<20s} {1:>5d}".format(club, score))
